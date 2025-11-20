c966a06f8349f7e103a03bdceadd2941bcd87851
Asim Aslam <asim@aslam.me>
2025-11-20 15:55:08 +0000

    x

--- home/home.go
+++ home/home.go
@@ -12,17 +12,19 @@ import (
 	"mu/video"
 )
 
-var Template = `<div id="home"><div id="home-layout">%s</div></div>`
+var Template = `<div id="home">%s</div>`
 
-func Cards(news, markets, reminder, latest string) []string {
+func Cards(news, markets, reminder, posts, latest string) []string {
 	news += app.Link("More", "/news")
 	markets += app.Link("More", "/markets")
+	posts += app.Link("More", "/blog")
 	latest += app.Link("More", "/video")
 
 	cards := []string{
 		app.Card("news", "News", news),
 		app.Card("reminder", "Reminder", reminder),
 		app.Card("markets", "Markets", markets),
+		app.Card("posts", "Posts", posts),
 		app.Card("video", "Video", latest),
 	}
 	return cards
@@ -69,28 +71,12 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	headlines := news.Headlines()
 	markets := news.Markets()
 	reminder := news.Reminder()
+	posts := blog.Preview()
 	latest := video.Latest()
 
-	// Get the posting form and limited feed
-	postForm := blog.PostingForm("/home")
-	homeFeed := blog.HomeFeed()
-
-	// Create the main feed column with More link
-	feedColumn := fmt.Sprintf(`<div id="feed-column">
-		<h2>Posts</h2>
-		%s
-		<div id="posts-list">%s</div>
-		<div style="text-align: center; margin-top: 30px;">
-			<a href="/blog" style="padding: 10px 20px; background: #333; color: white; border-radius: 5px; text-decoration: none; display: inline-block;">View All Posts</a>
-		</div>
-	</div>`, postForm, homeFeed)
-
-	// Create info cards sidebar
-	cards := strings.Join(Cards(headlines, markets, reminder, latest), "\n")
-	sidebar := fmt.Sprintf(`<div id="info-sidebar">%s</div>`, cards)
-	
-	// Combine feed and sidebar
-	homepage := fmt.Sprintf(Template, feedColumn+"\n"+sidebar)
+	// create homepage
+	cards := strings.Join(Cards(headlines, markets, reminder, posts, latest), "\n")
+	homepage := fmt.Sprintf(Template, cards)
 
 	// render html
 	html := app.RenderHTML("Home", "The Mu homescreen", homepage)
