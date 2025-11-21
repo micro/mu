cd636c2e9a0c25b0d68d2aec5fd1a6aac3d1f10c
Asim Aslam <asim@aslam.me>
2025-11-21 09:19:45 +0000

    x

--- app/html/mu.js
+++ app/html/mu.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER CONFIGURATION
 // ============================================
 var APP_PREFIX = 'mu_';
-var VERSION = 'v15';
+var VERSION = 'v16';
 var CACHE_NAME = APP_PREFIX + VERSION;
 
 // Static assets to cache on install
