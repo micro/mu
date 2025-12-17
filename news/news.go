d7bd2fd36fd541d54767cb5e33785ee7e732af20
Asim Aslam <asim@aslam.me>
2025-12-17 11:08:19 +0000

    x

--- news/news.go
+++ news/news.go
@@ -274,7 +274,7 @@ func getPrices() map[string]float64 {
 	return prices
 }
 
-var tickers = []string{"ETH", "BTC", "PAXG", "XLM", "GBP"}
+var tickers = []string{"GBP", "XLM", "ETH", "BTC", "PAXG"}
 
 var futures = map[string]string{
 	"OIL":      "CL=F",
