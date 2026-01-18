dc362c076d55265bdb84ab6f4da7642b8f788a7b
Asim <asim@mu.xyz>
2026-01-18 20:48:19 +0000

    Mobile #messages: increase offset to 320px
    Co-authored-by: Shelley <shelley@exe.dev>

--- app/html/mu.js
+++ app/html/mu.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER CONFIGURATION
 // ============================================
 var APP_PREFIX = 'mu_';
-var VERSION = 'v80';
+var VERSION = 'v81';
 var CACHE_NAME = APP_PREFIX + VERSION;
 
 // Minimal caching - only icons
