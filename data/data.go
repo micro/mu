458d4c4756673143bb023bb19221d381d05d58e3
Asim <asim@mu.xyz>
2026-01-22 17:15:30 +0000

    data: create subdirectories when saving files
    Co-authored-by: Shelley <shelley@exe.dev>

--- data/data.go
+++ data/data.go
@@ -133,7 +133,8 @@ func SaveFile(key, val string) error {
 	dir := os.ExpandEnv("$HOME/.mu")
 	path := filepath.Join(dir, "data")
 	file := filepath.Join(path, key)
-	os.MkdirAll(path, 0700)
+	// Create all parent directories including subdirectories in key
+	os.MkdirAll(filepath.Dir(file), 0700)
 	os.WriteFile(file, []byte(val), 0644)
 	return nil
 }
