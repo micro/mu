7ecc443cbe7a5db15ceec76505ef0e958fe46000
Asim <asim@mu.xyz>
2026-01-23 11:04:41 +0000

    Make home icon in topic nav larger and more clickable
    Co-authored-by: Shelley <shelley@exe.dev>

--- app/html/mu.js
+++ app/html/mu.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER CONFIGURATION
 // ============================================
 var APP_PREFIX = 'mu_';
-var VERSION = 'v122';
+var VERSION = 'v123';
 var CACHE_NAME = APP_PREFIX + VERSION;
 
 // Minimal caching - only icons
