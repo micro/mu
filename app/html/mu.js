8bbce7660522cc15833f8d4ec46b8a6b2931b725
Asim <asim@mu.xyz>
2026-01-23 10:46:40 +0000

    Add padding to nav for absolute nav-bottom space
    Co-authored-by: Shelley <shelley@exe.dev>

--- app/html/mu.js
+++ app/html/mu.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER CONFIGURATION
 // ============================================
 var APP_PREFIX = 'mu_';
-var VERSION = 'v114';
+var VERSION = 'v115';
 var CACHE_NAME = APP_PREFIX + VERSION;
 
 // Minimal caching - only icons
