ae62b600d27e01eba8ce5684e386d22adcf0e4b8
Asim <asim@mu.xyz>
2026-01-23 11:02:07 +0000

    Make signed in text smaller and greyer
    Co-authored-by: Shelley <shelley@exe.dev>

--- app/html/mu.js
+++ app/html/mu.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER CONFIGURATION
 // ============================================
 var APP_PREFIX = 'mu_';
-var VERSION = 'v121';
+var VERSION = 'v122';
 var CACHE_NAME = APP_PREFIX + VERSION;
 
 // Minimal caching - only icons
