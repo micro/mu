1be9a5b44b66b6487ce042bf587c0369062129a5
Asim Aslam <asim@aslam.me>
2026-02-15 13:39:11 +0000

    bump version

--- app/html/mu.js
+++ app/html/mu.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER CONFIGURATION
 // ============================================
 var APP_PREFIX = 'mu_';
-var VERSION = 'v135';
+var VERSION = 'v136';
 var CACHE_NAME = APP_PREFIX + VERSION;
 
 // Minimal caching - only icons
