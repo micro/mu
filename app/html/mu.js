beaa8f3ce2325388ed69b3626f25cc847a24bf3b
Asim Aslam <asim@aslam.me>
2026-02-19 11:46:33 +0000

    x

--- app/html/mu.js
+++ app/html/mu.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER CONFIGURATION
 // ============================================
 var APP_PREFIX = 'mu_';
-var VERSION = 'v146';
+var VERSION = 'v147';
 var CACHE_NAME = APP_PREFIX + VERSION;
 
 // Minimal caching - only icons
